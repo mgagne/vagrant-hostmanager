a41507247cd7b353ab1ed69a51b47ad8fb6d0420
Philippe Godin <pgodin@iweb.com>
2013-04-24 14:26:27 -0400

    Sort the content of expected and actual host file in the tests.
    We can't predict in which order vagrant-hostmanager handles the host file when appending to it.
    
    Reviewed-by: Mathieu Mitchell <mmitchell@iweb.com>

--- test/test.sh
+++ test/test.sh
@@ -10,8 +10,8 @@ servers="server1 server2"
 
 for server in ${servers}; do
   echo "[${server}] assert original output is what we expected"
-  actual=$(vagrant_ssh ${server} 'cat /etc/hosts')
-  expected=$(cat ${server}.prestine.hosts.file.txt)
+  actual=$(vagrant_ssh ${server} 'cat /etc/hosts' | sort)
+  expected=$(cat ${server}.prestine.hosts.file.txt | sort)
   [ "${actual}" == "${expected}" ] || fail "Original hosts file does not match what we expected for server ${server}"
 done
 
@@ -19,8 +19,8 @@ vagrant hostmanager
 
 for server in ${servers}; do
   echo "[${server}] assert output is what we expect after hostmanager"
-  actual=$(vagrant_ssh ${server} 'cat /etc/hosts')
-  expected=$(cat ${server}.expected.hosts.file.txt)
+  actual=$(vagrant_ssh ${server} 'cat /etc/hosts' | sort)
+  expected=$(cat ${server}.expected.hosts.file.txt | sort)
   [ "${actual}" == "${expected}" ] || fail "Hosts file after hostmanager modifications do not match for server ${server}"
 done
 
@@ -30,8 +30,8 @@ vagrant hostmanager
 
 for server in ${servers}; do
   echo "[${server}] assert output is what we expect after hostmanager was applied twice"
-  actual=$(vagrant_ssh ${server} 'cat /etc/hosts')
-  expected=$(cat ${server}.expected.hosts.file.txt)
+  actual=$(vagrant_ssh ${server} 'cat /etc/hosts' | sort)
+  expected=$(cat ${server}.expected.hosts.file.txt | sort)
   [ "${actual}" == "${expected}" ] || fail "Hosts file after hostmanager modifications (take 2) do not match for server ${server}"
 done
 
